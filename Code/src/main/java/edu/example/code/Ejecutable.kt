10f2e5771abee8744816c7e4109fa4f47e525006
samuelmrtz <isaacmtzley01@gmail.com>
2026-02-08 13:48:39 -0600

    EjemploSubclase

--- Code/src/main/java/edu/example/code/Ejecutable.kt
+++ Code/src/main/java/edu/example/code/Ejecutable.kt
@@ -1,9 +1,11 @@
 package edu.example.code
+import kotlin.math.PI
 
 open class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
     init {
         println("aquarium initializing")
     }
+
     open var volume: Int
         get() = width * height * length / 1000
         set(value) {
@@ -12,21 +14,37 @@ open class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int 
 
     open val shape = "rectangle"
 
-    open var water : Double = 0.0
+    open val water: Double
         get() = volume * 0.9
+
     fun printSize() {
-        println(
-            "Width: $width cm " +
-                    "Length: $length cm " +
-                    "Height: $height cm "
-        )
-        println("Volume: $volume liters Water: $water liters (${water / volume * 100.0}% full)")
+        println("-------------------------------")
+        println("Shape: $shape")
+        println("Width: $width cm | Length: $length cm | Height: $height cm")
+        println("Volume: $volume liters | Water: $water liters (${(water / volume) * 100.0}% full)")
     }
 }
 
+class TowerTank(height: Int, var diameter: Int) : Aquarium(height = height, width = diameter, length = diameter) {
+
+    override var volume: Int
+        get() = ((width / 2.0) * (length / 2.0) * height / 1000.0 * PI).toInt()
+        set(value) {
+            height = ((value * 1000.0 / PI) / ((width / 2.0) * (length / 2.0))).toInt()
+        }
+
+    override val water: Double
+        get() = volume * 0.8
+
+    override val shape = "cylinder"
+}
+
 fun buildAquarium() {
-    val aquarium6 = Aquarium(length = 25, width = 25, height = 40)
-    aquarium6.printSize()
+    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
+    myAquarium.printSize()
+
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
 }
 
 fun main() {
