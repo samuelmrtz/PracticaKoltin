989488b8f8156a73496d5b66ff5717fb9bcb0c1b
samuelmrtz <isaacmtzley01@gmail.com>
2026-02-08 13:35:16 -0600

    Constructores Clase Aquarium

--- Code/src/main/java/edu/example/code/Ejecutable.kt
+++ Code/src/main/java/edu/example/code/Ejecutable.kt
@@ -1,23 +1,30 @@
 package edu.example.code
 
-class Aquarium {
-    var width: Int = 20
-    var height: Int = 40
-    var length: Int = 100
+class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+    init {
+        println("aquarium initializing")
+    }
+    val volume: Int
+        get() = width * height * length / 1000  // 1000 cm^3 = 1 liter
     fun printSize() {
         println(
             "Width: $width cm " +
                     "Length: $length cm " +
                     "Height: $height cm "
         )
+        println("Volume: $volume liters")
     }
 }
 
 fun buildAquarium() {
-    val myAquarium = Aquarium()
-    myAquarium.printSize()
-    myAquarium.height = 60
-    myAquarium.printSize()
+    val aquarium1 = Aquarium()
+    aquarium1.printSize()
+    val aquarium2 = Aquarium(width = 25)
+    aquarium2.printSize()
+    val aquarium3 = Aquarium(height = 35, length = 110)
+    aquarium3.printSize()
+    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+    aquarium4.printSize()
 }
 
 fun main() {
