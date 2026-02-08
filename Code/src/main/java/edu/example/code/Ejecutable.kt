f0191606f6c4aa43cf1ee65b414e7fff0685651b
samuelmrtz <isaacmtzley01@gmail.com>
2026-02-08 13:41:06 -0600

    Sublases y herencias

--- Code/src/main/java/edu/example/code/Ejecutable.kt
+++ Code/src/main/java/edu/example/code/Ejecutable.kt
@@ -1,30 +1,32 @@
 package edu.example.code
 
-class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+open class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
     init {
         println("aquarium initializing")
     }
-    val volume: Int
-        get() = width * height * length / 1000  // 1000 cm^3 = 1 liter
+    open var volume: Int
+        get() = width * height * length / 1000
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
+
+    open val shape = "rectangle"
+
+    open var water : Double = 0.0
+        get() = volume * 0.9
     fun printSize() {
         println(
             "Width: $width cm " +
                     "Length: $length cm " +
                     "Height: $height cm "
         )
-        println("Volume: $volume liters")
+        println("Volume: $volume liters Water: $water liters (${water / volume * 100.0}% full)")
     }
 }
 
 fun buildAquarium() {
-    val aquarium1 = Aquarium()
-    aquarium1.printSize()
-    val aquarium2 = Aquarium(width = 25)
-    aquarium2.printSize()
-    val aquarium3 = Aquarium(height = 35, length = 110)
-    aquarium3.printSize()
-    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
-    aquarium4.printSize()
+    val aquarium6 = Aquarium(length = 25, width = 25, height = 40)
+    aquarium6.printSize()
 }
 
 fun main() {
